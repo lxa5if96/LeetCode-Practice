9bfd6d921ac96247b4fd6bcee2f3f9d562fed6ef
ASIF ALAM ANSARI <aastatus945@gmail.com>
2026-08-05 21:48:10 +0530

    Time: 2 ms (73.45%), Space: 19.2 MB (57.43%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,14 +1,9 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 0 :
+        if x == 0:
             return 0
-        elif x == 1 or x == 2:
-            return 1
-
-        for i in range(x):
-            sr = i * i
-            if sr == x:
-                return i
-            elif sr > x:
-                return i - 1
+        sr = x
+        while sr * sr > x:
+            sr = (sr + x // sr) // 2
         
+        return sr
